0058a9fb2e3e6df5e961c32bd236b42fc7166dd4
ravikant <Ravikantwaghmare@vionsys.com>
2025-04-28 13:00:49 +0530

    feat: Prisma setup done

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
-import DotenvFlow from 'dotenv-flow';
-DotenvFlow.config();
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 export default {
     ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL
 };
