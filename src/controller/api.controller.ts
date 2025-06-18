0c857c2debd69ac5440a1dfe19ffdb9bbcaa0012
ravikant <Ravikantwaghmare@vionsys.com>
2025-06-18 14:43:56 +0530

    fix: Update API response message to indicate server health status

--- src/controller/api.controller.ts
+++ src/controller/api.controller.ts
@@ -3,6 +3,6 @@ import httpResponse from '../util/httpResponse';
 
 export default {
     get: (req: Request, res: Response) => {
-        return httpResponse(req, res, 200, 'API is running successfully', {});
+        return httpResponse(req, res, 200, 'API version 1: Healthy server!!', {});
     }
 };
